202412431ec0c134f03a5d5b10eac3d3df52b17c
Megan Russell <megan.russell50@gmail.com>
2021-02-06 11:50:47 -0800

    pushing updates to js

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@ const googleMapKey = 'AIzaSyCa_GtE78cio4hq8ZlvVZEVYEFQEgdOboQ';
 const googleMapID = 'ChIJVTPokywQkFQRmtVEaUZlJRA'
 var charityNavigatorURL = "https://api.data.charitynavigator.org/v2/Organizations?app_id=1f1b3eac&app_key=3ec0b4533e236b7a527fbf81dd68dd75&state=WA&city=Seattle&zip=98105"
 var googleMapURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyCa_GtE78cio4hq8ZlvVZEVYEFQEgdOboQ'
-var startButton = document.querySelector(".btn")
+var startButton = document.getElementById("search-btn")
 var appendList = document.querySelector(".charity")
 
 startButton.addEventListener("click", function(){
@@ -23,6 +23,19 @@ startButton.addEventListener("click", function(){
         var charityAddress = data[0].mailingAddress.streetAddress1;
         var charityCity = data[0].mailingAddress.city;
         var charityClassification = data[0].irsClassification.nteeClassification;
+        var selectedCause = document.getElementById("cause").value;
+        console.log(selectedCause)
+        var filteredCharities = []
+
+
+        for ( var i = 0; i<data.length; i++) {
+
+            if (selectedCause === data[i].irsClassification.nteeClassification){
+                filteredCharities.push(data[i])
+            }
+        }
+
+        console.log(filteredCharities)
 
         var newListElm = document.createElement("li");
 
@@ -30,4 +43,6 @@ startButton.addEventListener("click", function(){
             
             appendList.appendChild(newListElm)
      })
+
+     
 })
